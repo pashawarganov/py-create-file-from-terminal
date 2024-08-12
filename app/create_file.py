fd1b34720f8e8b8581e836f4bd3c983e67ddc17b
Pasha <pavik.varganov@gmail.com>
2024-08-12 21:48:23 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,23 @@
-# write your code here
+import sys
+import os
+import datetime
+
+
+if "-d" in sys.argv and "-f" in sys.argv:
+    directories = sys.argv[sys.argv.index("-d") + 1:sys.argv.index("-f")]
+    path = os.path.join(*directories)
+    os.makedirs(path)
+
+    file_path = os.path.join(path, sys.argv[sys.argv.index("-f") + 1])
+else:
+    file_path = sys.argv[sys.argv.index("-f") + 1]
+
+with open(file_path, "a") as file:
+    file.write(str(datetime.datetime.now()) + "\n")
+    while True:
+        content = input("Enter content line: ")
+        if content.lower() == "stop":
+            break
+        file.write(content + "\n")
+
+    file.write("\n")
