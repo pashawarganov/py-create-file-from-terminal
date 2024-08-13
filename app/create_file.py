47c525d7ccacb424f8c175f047f737e643c26bce
Pasha <pavik.varganov@gmail.com>
2024-08-13 16:09:48 +0300

    Solution 2 Separate code into functions, add blank line if command == "stop", change Date time format

--- app/create_file.py
+++ app/create_file.py
@@ -1,23 +1,29 @@
 import sys
 import os
-import datetime
+from datetime import datetime
 
 
-if "-d" in sys.argv and "-f" in sys.argv:
-    directories = sys.argv[sys.argv.index("-d") + 1:sys.argv.index("-f")]
-    path = os.path.join(*directories)
-    os.makedirs(path)
+def create_path() -> str:
+    if "-d" in sys.argv and "-f" in sys.argv:
+        directories = sys.argv[sys.argv.index("-d") + 1:sys.argv.index("-f")]
+        path = os.path.join(*directories)
+        os.makedirs(path)
 
-    file_path = os.path.join(path, sys.argv[sys.argv.index("-f") + 1])
-else:
-    file_path = sys.argv[sys.argv.index("-f") + 1]
+        return os.path.join(path, sys.argv[sys.argv.index("-f") + 1])
+    return sys.argv[sys.argv.index("-f") + 1]
 
-with open(file_path, "a") as file:
-    file.write(str(datetime.datetime.now()) + "\n")
-    while True:
-        content = input("Enter content line: ")
-        if content.lower() == "stop":
-            break
-        file.write(content + "\n")
 
-    file.write("\n")
+def create_file() -> None:
+    file_path = create_path()
+
+    with open(file_path, "a") as file:
+        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+
+        line_number = 1
+        while True:
+            content = input("Enter content line: ")
+            if content.lower() == "stop":
+                file.write("\n")
+                break
+            file.write(str(line_number) + " " + content + "\n")
+            line_number += 1
