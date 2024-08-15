69fe12f065f155b6f4b61adff87f2db4a1a44a71
Pasha <pavik.varganov@gmail.com>
2024-08-15 17:18:01 +0300

    Solution 4

--- app/create_file.py
+++ app/create_file.py
@@ -7,30 +7,36 @@ import os
 def create_path(directories: list | None, filename: str | None) -> str:
     if directories:
         path_dir = os.path.join(*directories)
-        try:
-            os.makedirs(path_dir)
-            print("<<<Directories created>>>")
-        except FileExistsError:
-            print("<<<Directories already exists>>>")
+        os.makedirs(path_dir, exist_ok=True)
 
         if filename:
             return os.path.join(path_dir, filename)
     return filename
 
 
-def create_file(file_path: str) -> None:
-    with open(file_path, "a") as file:
-        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+def make_content() -> list:
+    content_list = [datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n", ]
+    line_number = 1
+    while True:
+        content = input("Enter content line: ")
+        if content.lower() == "stop":
+            content_list.append("\n")
+            break
+        content_list.append(str(line_number) + " " + content + "\n")
+        line_number += 1
 
-        line_number = 1
-        while True:
-            content = input("Enter content line: ")
-            if content.lower() == "stop":
-                file.write("\n")
-                break
-            file.write(str(line_number) + " " + content + "\n")
-            line_number += 1
-    print("<<<File created>>>")
+    return content_list
+
+
+def create_file(file_path: str) -> None:
+    content = make_content()
+    try:
+        user_file = open(file_path, "a")
+        user_file.writelines(content)
+    except Exception as e:
+        print(e)
+    finally:
+        user_file.close()
 
 
 def main() -> None:
@@ -43,7 +49,6 @@ def main() -> None:
     )
     parser.add_argument("-f", "--file", type=str, help="Filename")
     args = parser.parse_args()
-    print(args)
     path = create_path(args.directories, args.file)
 
     if args.file:
